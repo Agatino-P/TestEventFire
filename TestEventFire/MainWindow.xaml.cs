6a3fec8f99fcfa25433aff4f1af5a238936e709e
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-05-04 23:20:18 +0200

    Add project files.

---
+++ TestEventFire/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows;
+
+namespace TestEventFire
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    internal class EventOneArgs : EventArgs
+    {
+        public string Testo { get; set; }
+        public EventOneArgs(string testo)
+        {
+            Testo = testo;
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+
+        private bool alreadySubscribed = false;
+
+        private event EventHandler<EventOneArgs> EventOne;// = delegate { };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //EventOne += OnEventOne;
+        }
+
+        private void OnEventOne(object sender, EventOneArgs e)
+        {
+            if (sender == null)
+            {
+                return;
+            }
+
+            System.Diagnostics.Debug.Write($"OnEventOne - {DateTime.Now}{Environment.NewLine}");
+            addTimedLogText($"Received event with arg={e.Testo}");
+        }
+
+        private void btnRaiseEvent_Click(object sender, RoutedEventArgs e)
+        {
+            addTimedLogText("Raising Event with arg={txtTesto.Text}");
+            if (EventOne != null)
+            {
+                EventOne(this, new EventOneArgs(txtTesto.Text));
+            }
+        }
+
+        private void btnSubscribeEvent_Click(object sender, RoutedEventArgs e)
+        {
+            subscribeOnce();
+        }
+        private void subscribeOnce()
+        {
+            if (alreadySubscribed)
+            {
+                return;
+            }
+
+            addTimedLogText("Subscribing");
+            alreadySubscribed = true;
+            EventOne += OnEventOne;
+        }
+
+        private void btnUnSubscribeEvent_Click(object sender, RoutedEventArgs e)
+        {
+            if (!alreadySubscribed)
+            {
+                return;
+            }
+
+            addTimedLogText("UnSubscribing");
+            alreadySubscribed = false;
+            EventOne -= OnEventOne;
+
+        }
+
+        private void btnResetLog_Click(object sender, RoutedEventArgs e)
+        {
+            txtLog.Text = "";
+            addTimedLogText("ResetLog");
+        }
+
+        private void addTimedLogText(string testo)
+        {
+            txtLog.Text += $"{Environment.NewLine}{DateTime.Now} - {testo}";
+        }
+    }
+}
